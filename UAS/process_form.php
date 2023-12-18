9bc71c67d34a35e1a678cdbf989d40d141cbccb9
166Taqy <130922472+166Taqy@users.noreply.github.com>
2023-12-18 09:08:17 +0700

    process_form.php

--- UAS/process_form.php
+++ UAS/process_form.php
@@ -5,15 +5,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $checkbox = isset($_POST['check']) ? 'Checked' : 'Unchecked';
     $radio = isset($_POST['radio']) ? $_POST['radio'] : 'N/A';
 
-    // Perform server-side validation here
-
     // Display processed data
     echo "<p>Name: $name</p>";
     echo "<p>Email: $email</p>";
     echo "<p>Checkbox: $checkbox</p>";
     echo "<p>Radio: $radio</p>";
 
-    // Save to the database and log browser information and IP address
-    // Database connection and query execution code here
 }
 ?>
